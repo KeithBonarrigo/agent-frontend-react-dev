f7c96d8b0e06cc94608acb1c22952a6943fbac4c
Keith Bonarrigo <kabonarrigo@gmail.com>
2026-02-26 17:02:18 -0700

    adding propel domain conditionals in UI

--- src/App.jsx
+++ src/App.jsx
@@ -18,37 +18,48 @@ import PasswordResetConfirm from "./Pages/PasswordResetConfirm";
 import OAuthCallback from "./Pages/OAuthCallback";
 import Agent from "./Pages/Agent";
 import { UserProvider } from './contexts/UserContext';
-import { DomainProvider } from './contexts/DomainContext';
+import { DomainProvider, useDomain } from './contexts/DomainContext';
+
+function AppRoutes() {
+  const { domainInfo } = useDomain();
+  const hostname = domainInfo?.hostname || window.location.hostname;
+  const isPropel = hostname === 'localhost' || hostname?.includes('propel');
+
+  return (
+    <>
+      <LanguageSelector fixed={true} />
+      <Routes>
+        <Route element={<HomeLayout />}>
+          <Route path="/" element={isPropel ? <Agent /> : <Home />} />
+          <Route path="/data-deletion" element={<DataDeletion />} />
+          <Route path="/cookies" element={<CookiePolicy />} />
+          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Route>
+        <Route element={<HomeLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/payment" element={<Checkout />} />
+          <Route path="/password-reset/request" element={<PasswordResetRequest />} />
+          <Route path="/password-reset/check-email" element={<PasswordResetCheckEmail />} />
+          <Route path="/password-reset/confirm" element={<PasswordResetConfirm />} />
+          <Route path="/agent" element={<Agent />} />
+          <Route path="/oauth/callback" element={<OAuthCallback />} />
+          <Route path="/oauth/:provider/callback" element={<OAuthCallback />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <DomainProvider>
       <UserProvider>
         <BrowserRouter>
-        <LanguageSelector fixed={true} />
-        <Routes>
-          <Route element={<HomeLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/data-deletion" element={<DataDeletion />} />
-            <Route path="/cookies" element={<CookiePolicy />} />
-            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-            <Route path="/privacy" element={<Privacy />} />
-
-          </Route>
-          <Route element={<HomeLayout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/payment" element={<Checkout />} />
-            <Route path="/password-reset/request" element={<PasswordResetRequest />} />
-            <Route path="/password-reset/check-email" element={<PasswordResetCheckEmail />} />
-            <Route path="/password-reset/confirm" element={<PasswordResetConfirm />} />
-            <Route path="/agent" element={<Agent />} />
-            <Route path="/oauth/callback" element={<OAuthCallback />} />
-            <Route path="/oauth/:provider/callback" element={<OAuthCallback />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </UserProvider>
+          <AppRoutes />
+        </BrowserRouter>
+      </UserProvider>
     </DomainProvider>
   );
 }
